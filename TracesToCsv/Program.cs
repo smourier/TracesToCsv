7e5b24ebfbab3b8253eb6241a5da9b5cdf51a6a9
Simon Mourier <smourier@users.noreply.github.com>
2025-09-19 15:51:16 +0200

    wip

--- TracesToCsv/Program.cs
+++ TracesToCsv/Program.cs
@@ -26,6 +26,7 @@ public class Program
         app.UseMiddleware<ApiExceptionHandlingMiddleware>();
         app.UseHttpsRedirection();
         app.UseAuthorization();
+        app.UseStaticFiles();
         app.MapControllers();
         app.MapRazorPages();
         app.Run();
